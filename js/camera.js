a4d9bc8afd0a2e648e689cff3d77c8cde27381c8
Karol Kowalski <karol.kowalski@eyedea.io>
2016-01-18 18:37:30 +0100

    Style improvements

--- js/camera.js
+++ js/camera.js
@@ -5,6 +5,8 @@ upload_image,
 video = document.getElementsByTagName('video')[0],
 heading = document.getElementsByTagName('h1')[0];
 
+var active = false;
+
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
 var ws = new WebSocket(ws_protocol+"://"+ws_ip+":"+ws_port+"/");
@@ -20,7 +22,7 @@ function capture(video, scaleFactor) {
     return canvas;
 }
 
-function shoot(){
+function shoot() {
     var output = document.getElementById('output');
     var canvas = capture(video, scaleFactor);
     canvas.onclick = function(){
@@ -35,12 +37,67 @@ function shoot(){
     $.mobile.changePage('#photos');
 }
 
-function supportsToDataURL()
-{
+function supportsToDataURL() {
     var c = document.createElement("canvas");
     var data = c.toDataURL("image/png");
     return (data.indexOf("data:image/png") == 0);
 }
+
+button = document.getElementById('button');
+
+ws.onopen = function() {
+    console.log('Websocket Open');
+};
+
+ws.onclose = function() {
+    console.log('Websocket Closed');
+};
+
+ws.onerror = function(event) {
+    console.log('error');
+};
+
+ws.onmessage = function(event) {
+    console.log('message');
+};
+
+ws.ontext = function(event) {
+    console.log('text');
+};
+
+document.getElementById("button").addEventListener('click', changeActiveState, false);
+
+function changeActiveState() {
+  if(active){
+    active=false;
+    document.getElementById("button").value="Start Streaming";
+  }else{
+    active=true;
+    document.getElementById("button").value="Stop Streaming";
+    streamFrame();
+  }
+}
+
+function streamFrame(){
+
+    var context = canvas.getContext("2d");
+    var cameraName = document.getElementsByName("BoxCameraName")[0].value;
+    var RadiocameraType = document.getElementsByName("BoxCameraType")[0];
+        var cameraType = "";
+        for (var i = 0; i < RadiocameraType.length; i++) {
+            if (RadiocameraType[i].checked) {
+                cameraType = RadiocameraType[i].value;
+            }
+        }
+
+    setInterval(function(){
+      if(active){
+        context.drawImage(video, 0, 0, 240, 320);
+        var image = canvas.toDataURL("image/png");
+        ws.send(JSON.stringify({ cameraType: cameraType , cameraName: cameraName, cameraFrame: image }));
+      }
+    },400);
+  }
 
 navigator.getUserMedia = (navigator.getUserMedia ||
                           navigator.webkitGetUserMedia ||
@@ -63,54 +120,6 @@ navigator.getUserMedia = (navigator.getUserMedia ||
       alert('Sorry, the browser you are using doesn\'t support getUserMedia');
     }
 
-if(!supportsToDataURL())
-{
+if(!supportsToDataURL()) {
     heading.textContent+="You browser does NOT support Canvas.toDataURL();"
-}
-
-
-button = document.getElementById('button');
-
-ws.onopen = function()
-{
-    console.log('Websocket Open');
-};
-
-ws.onclose = function()
-{
-    console.log('Websocket Closed');
-};
-
-ws.onerror = function(event)
-{
-    console.log('error');
-};
-
-ws.onmessage = function(event)
-{
-    console.log('message');
-};
-
-ws.ontext = function(event)
-{
-    console.log('text');
-};
-
-addEventListener('click', streamFrame, false);
-
-function streamFrame()
-{
-  var context = canvas.getContext("2d");
-  var cameraName = document.getElementsByName("BoxCameraName")[0].value;
-  var cameraType = document.getElementsByName("BoxCameraType")[0].value;
-
-  setInterval(function(){
-      context.drawImage(video, 0, 0, 240, 320);
-      var image = canvas.toDataURL("image/png");
-
-      // Camera debug option
-      //console.log(JSON.stringify({ cameraType: cameraType , cameraName: cameraName, cameraFrame: image }));
-
-      ws.send(JSON.stringify({ cameraType: cameraType , cameraName: cameraName, cameraFrame: image }));
-  },400);
 }
